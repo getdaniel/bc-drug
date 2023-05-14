0b29f633c26e02a5e3dbd0af828597a2cb135d5e
Me <getdaniel>
2023-05-14 23:44:04 +0300

    increase width

--- app.py
+++ app.py
@@ -59,8 +59,8 @@ st.markdown(
     """
    <style>
         [data-testid="stSidebar"][aria-expanded="true"]{
-           min-width: 160px;
-           max-width: 150px;
+           min-width: 200px;
+           max-width: 200px;
        }
     """,
     unsafe_allow_html=True,
