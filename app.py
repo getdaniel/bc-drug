abc3274e330ab165dc311f13b022f7540b966d3c
Me <getdaniel>
2023-05-16 00:49:26 +0300

    return to the original

--- app.py
+++ app.py
@@ -1,116 +1,10 @@
-# import streamlit as st
-# import subprocess
-# import base64
-# import os
-# import pickle
-# import pandas as pd
-# from PIL import Image
-
-# # Molecular descriptor calculator
-# def desc_calc():
-#     # Performs the descriptor calculation
-#     bashCommand = "java -Xms2G -Xmx2G -Djava.awt.headless=true -jar ./ML/PaDEL-Descriptor/PaDEL-Descriptor.jar -removesalt -standardizenitro -fingerprints -descriptortypes ./ML/PaDEL-Descriptor/PubchemFingerprinter.xml -dir ./ -file ML/descriptors_output.csv"
-#     process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-#     output, error = process.communicate()
-#     os.remove('ML/molecule.smi')
-
-# # File download
-# def filedownload(df):
-#     csv = df.to_csv(index=False)
-#     # strings <-> bytes conversions
-#     b64 = base64.b64encode(csv.encode()).decode()
-#     href = f'<a href="data:file/csv;base64,{b64}" download="prediction.csv">Download Predictions</a>'
-#     return href
-
-# # Model building
-# def build_model(input_data):
-#     # Reads in saved regression model
-#     load_model = pickle.load(open('ML/aromatase.pkl', 'rb'))
-#     # Apply model to make predictions
-#     prediction = load_model.predict(input_data)
-#     st.header('**Prediction output**')
-#     prediction_output = pd.Series(prediction, name='pIC50')
-#     molecule_name = pd.Series(load_data[1], name='molecule_name')
-#     df = pd.concat([molecule_name, prediction_output], axis=1)
-#     st.write(df)
-#     st.markdown(filedownload(df), unsafe_allow_html=True)
-
-# # Set page title and icon
-# st.set_page_config(page_title="Drug Discovery",
-#                    page_icon="assets/images/bio_logo.png")
-
-# # Set title to be centered
-# st.markdown("<h2 style='text-align: center;'>Drug Discovery Using AI for Breast Cancer</h2>",
-#             unsafe_allow_html=True)
-
-# # Add image
-# image = Image.open("assets/images/logo.png")
-# st.image(image, use_column_width=True)
-
-# # Side bar
-# st.markdown(
-#     """
-#    <style>
-#         [data-testid="stSidebar"][aria-expanded="true"]{
-#            min-width: 200px;
-#            max-width: 200px;
-#        }
-#     """,
-#     unsafe_allow_html=True,
-# )
-
-# # Add buttons to sidebar
-# st.sidebar.button("New Web", use_container_width=True)
-# st.sidebar.button("Settings", use_container_width=True)
-# st.sidebar.button("Feedback", use_container_width=True)
-# st.sidebar.button("History", use_container_width=True)
-# st.sidebar.button("Log Out", use_container_width=True)
-
-# # Input file upload section
-# uploaded_file = st.file_uploader("Upload your input file (.txt)", type=['txt'])
-# st.markdown("[Example input file](https://raw.githubusercontent.com/getdaniel/bc-drug/main/ML/aromatase_exp.txt)")
-
-# # Display a message if no file is uploaded
-# if uploaded_file is None:
-#     st.info("No file uploaded or file is empty. Please upload a file.")
-
-# # Prediction section
-# if st.button("Predict", use_container_width=True) and uploaded_file is not None:
-#     # Read input file
-#     load_data = pd.read_table(uploaded_file, sep=' ', header=None)
-#     load_data.to_csv('ML/molecule.smi', sep='\t', header=False, index=False)
-
-#     # Display original input data
-#     st.header('**Input Data with Dataframe**')
-#     st.write(load_data)
-
-#     # Calculate descriptors
-#     with st.spinner("Calculating Descriptors..."):
-#         desc_calc()
-
-#     # Display calculated descriptors
-#     st.header('**Calculated Molecular Descriptors**')
-#     desc = pd.read_csv('ML/descriptors_output.csv')
-#     st.write(desc)
-#     st.write(desc.shape)
-
-#     # Read descriptor list used in previously built model
-#     st.header('**Subset of Descriptors from the Models**')
-#     Xlist = list(pd.read_csv('ML/descriptor_list.csv').columns)
-#     desc_subset = desc[Xlist]
-#     st.write(desc_subset)
-#     st.write(desc_subset.shape)
-
-#     # Apply trained model to make prediction
-#     build_model(desc_subset)
-
 import streamlit as st
+import subprocess
+import base64
+import os
+import pickle
 import pandas as pd
 from PIL import Image
-import subprocess
-import os
-import base64
-import pickle
 
 # Molecular descriptor calculator
 def desc_calc():
@@ -123,7 +17,8 @@ def desc_calc():
 # File download
 def filedownload(df):
     csv = df.to_csv(index=False)
-    b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
+    # strings <-> bytes conversions
+    b64 = base64.b64encode(csv.encode()).decode()
     href = f'<a href="data:file/csv;base64,{b64}" download="prediction.csv">Download Predictions</a>'
     return href
 
@@ -140,54 +35,71 @@ def build_model(input_data):
     st.write(df)
     st.markdown(filedownload(df), unsafe_allow_html=True)
 
-# Logo image
-image = Image.open('assets/images/logo.png')
+# Set page title and icon
+st.set_page_config(page_title="Drug Discovery",
+                   page_icon="assets/images/bio_logo.png")
 
+# Set title to be centered
+st.markdown("<h2 style='text-align: center;'>Drug Discovery Using AI for Breast Cancer</h2>",
+            unsafe_allow_html=True)
+
+# Add image
+image = Image.open("assets/images/logo.png")
 st.image(image, use_column_width=True)
 
-# Page title
-st.markdown("""
-# Bioactivity Prediction App (Acetylcholinesterase)
+# Side bar
+st.markdown(
+    """
+   <style>
+        [data-testid="stSidebar"][aria-expanded="true"]{
+           min-width: 200px;
+           max-width: 200px;
+       }
+    """,
+    unsafe_allow_html=True,
+)
 
-This app allows you to predict the bioactivity towards inhibting the `Acetylcholinesterase` enzyme. `Acetylcholinesterase` is a drug target for Alzheimer's disease.
+# Add buttons to sidebar
+st.sidebar.button("New Web", use_container_width=True)
+st.sidebar.button("Settings", use_container_width=True)
+st.sidebar.button("Feedback", use_container_width=True)
+st.sidebar.button("History", use_container_width=True)
+st.sidebar.button("Log Out", use_container_width=True)
 
-**Credits**
-- App built in `Python` + `Streamlit` by [Chanin Nantasenamat](https://medium.com/@chanin.nantasenamat) (aka [Data Professor](http://youtube.com/dataprofessor))
-- Descriptor calculated using [PaDEL-Descriptor](http://www.yapcwsoft.com/dd/padeldescriptor/) [[Read the Paper]](https://doi.org/10.1002/jcc.21707).
----
-""")
+# Input file upload section
+uploaded_file = st.file_uploader("Upload your input file (.txt)", type=['txt'])
+st.markdown("[Example input file](https://raw.githubusercontent.com/getdaniel/bc-drug/main/ML/aromatase_exp.txt)")
 
-# Sidebar
-with st.sidebar.header('1. Upload your CSV data'):
-    uploaded_file = st.sidebar.file_uploader("Upload your input file", type=['txt'])
-    st.sidebar.markdown("""
-[Example input file](https://raw.githubusercontent.com/dataprofessor/bioactivity-prediction-app/main/example_acetylcholinesterase.txt)
-""")
+# Display a message if no file is uploaded
+if uploaded_file is None:
+    st.info("No file uploaded or file is empty. Please upload a file.")
 
-if st.sidebar.button('Predict'):
+# Prediction section
+if st.button("Predict", use_container_width=True) and uploaded_file is not None:
+    # Read input file
     load_data = pd.read_table(uploaded_file, sep=' ', header=None)
-    load_data.to_csv('ML/molecule.smi', sep = '\t', header = False, index = False)
+    load_data.to_csv('ML/molecule.smi', sep='\t', header=False, index=False)
 
-    st.header('**Original input data**')
+    # Display original input data
+    st.header('**Input Data with Dataframe**')
     st.write(load_data)
 
-    with st.spinner("Calculating descriptors..."):
+    # Calculate descriptors
+    with st.spinner("Calculating Descriptors..."):
         desc_calc()
 
-    # Read in calculated descriptors and display the dataframe
-    st.header('**Calculated molecular descriptors**')
+    # Display calculated descriptors
+    st.header('**Calculated Molecular Descriptors**')
     desc = pd.read_csv('ML/descriptors_output.csv')
     st.write(desc)
     st.write(desc.shape)
 
     # Read descriptor list used in previously built model
-    st.header('**Subset of descriptors from previously built models**')
+    st.header('**Subset of Descriptors from the Models**')
     Xlist = list(pd.read_csv('ML/descriptor_list.csv').columns)
     desc_subset = desc[Xlist]
     st.write(desc_subset)
     st.write(desc_subset.shape)
 
-    # Apply trained model to make prediction on query compounds
+    # Apply trained model to make prediction
     build_model(desc_subset)
-else:
-    st.info('Upload input data in the sidebar to start!')
