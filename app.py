b2598a81627a33683c1660dc0edf3838cdb3d2ee
Me <getdaniel>
2023-05-17 13:42:28 +0300

    remove version num

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def filedownload(df):
     csv = df.to_csv(index=False)
     # strings <-> bytes conversions
     b64 = base64.b64encode(csv.encode()).decode()
-    href = f'<a href="data:file/csv;base64,{b64}" download="prediction.csv">Download Predictions</a>'
+    href = f'<a href="data:file/csv;base64,{b64}" download="prediction.csv">Download Prediction</a>'
     return href
 
 # Model building
