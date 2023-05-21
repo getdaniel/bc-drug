ae2185f9a0c981fb041b97a9e8691705522d0878
Me <getdaniel>
2023-05-21 23:38:46 +0300

    try to adapt text with dark mode

--- source/feedback.py
+++ source/feedback.py
@@ -3,6 +3,17 @@ import streamlit as st
 
 def handle_user_feedback(modal):
     if modal.is_open():
+        st.markdown(
+            """
+            <style>
+            /* Adjust the color and background-color properties as needed */
+            .stTextInput, .stTextArea, .stButton {
+                color: #ffffff !important;
+                background-color: #000000 !important;
+            }
+            </style>
+            """
+        )
         with modal.container():
             email = st.text_input("Email")
             message = st.text_area("Message")
