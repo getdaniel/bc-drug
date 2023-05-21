b3d637e03f7b69a2769cbeda2d16009095d638cf
Me <getdaniel>
2023-05-21 23:47:14 +0300

    update the css to experimental

--- source/feedback.py
+++ source/feedback.py
@@ -3,22 +3,10 @@ import streamlit as st
 
 def handle_user_feedback(modal):
     if modal.is_open():
-        st.markdown(
-            """
-            <style>
-            /* Adjust the color and background-color properties as needed */
-            .modal-content .stTextInput, .modal-content .stTextArea {
-                color: #ffffff !important;
-                background-color: #000000 !important;
-            }
-            .modal-content .stButton button {
-                color: #ffffff !important;
-                background-color: #1e88e5 !important;
-            }
-            </style>
-            """
-        )
         with modal.container():
+            # Force light mode temporarily
+            st.experimental_set_query_params(_forced_theme='light')
+            
             email = st.text_input("Email")
             message = st.text_area("Message")
             if st.button("Submit", use_container_width=True):
