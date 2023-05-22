c71b0f24bbdd06161d769905e57b0e088687e08e
Me <getdaniel>
2023-05-23 00:04:55 +0300

    move and image to the landing

--- app.py
+++ app.py
@@ -18,10 +18,6 @@ st.set_page_config(page_title="Drug Discovery",
 st.markdown("<h2 style='text-align: center;'>Drug Discovery Using AI for Breast Cancer</h2>",
             unsafe_allow_html=True)
 
-# Add image
-image = Image.open("assets/images/logo.png")
-st.image(image, use_column_width=True)
-
 # aromatase structure
 st.markdown(
     """
