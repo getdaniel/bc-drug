27541c4f7f1d2d7fcfb5864456e4d2292ea67bb4
Me <getdaniel>
2023-05-22 14:52:31 +0300

    try to bold labels

--- source/model.py
+++ source/model.py
@@ -15,8 +15,7 @@ red_color = '#FF0000'
 # Model building
 def build_model(load_data, input_data):
     if input_data.shape[0] == 0:
-        st.info(
-            "Input data has zero samples/ No descriptor Value. Please provide valid input data.")
+        st.info("Input data has zero samples/ No descriptor Value. Please provide valid input data.")
     else:
         # Reads in saved regression model
         load_model = pickle.load(open('ML/aromatase.pkl', 'rb'))
@@ -26,9 +25,9 @@ def build_model(load_data, input_data):
         st.header('**Prediction Output**')
 
         # Create a dataframe with molecule_name, pIC50 and SMILES
-        molecule_name = pd.Series(load_data[1], name='**molecule_name**')
-        smiles = pd.Series(load_data[0], name='**smiles**')
-        prediction_output = pd.Series(prediction, name='**pIC50**')
+        molecule_name = pd.Series(load_data[1], name='molecule_name')
+        smiles = pd.Series(load_data[0], name='smiles')
+        prediction_output = pd.Series(prediction, name='pIC50')
         df = pd.concat([molecule_name, smiles, prediction_output], axis=1)
 
         # Display the dataframe
@@ -36,7 +35,7 @@ def build_model(load_data, input_data):
 
         # Display the 3D structure for each chemical
         for index, row in df.iterrows():
-            st.subheader(f"Chemical: {row['molecule_name']}")
+            st.subheader(f"Chemical: **{row['molecule_name']}**")  # Bold text using Markdown
 
             # Replace with your own implementation to load the 3D structure
             mol = Chem.MolFromSmiles(row['smiles'])
@@ -49,13 +48,13 @@ def build_model(load_data, input_data):
 
         # Draw a bar chart with molecule_name as X-axis and prediction_output as Y-axis
         st.header('**Graphical Prediction Output**')
-        chart_data = df.set_index('**molecule_name**')
+        chart_data = df.set_index('molecule_name')
 
         # Create a new column for bar color based on pIC50 values
         chart_data['color'] = chart_data['pIC50'].apply(lambda x: green_color if x >= 6.5 else (yellow_color if 4.5 <= x < 6.5 else red_color))
 
         # Create a bar chart using plotly express
-        fig = px.bar(chart_data, x=chart_data.index, y='**pIC50**', color='color')
+        fig = px.bar(chart_data, x=chart_data.index, y='pIC50', color='color')
 
         # Customize the chart appearance
         fig.update_layout(showlegend=False)  # Hide the color legend
