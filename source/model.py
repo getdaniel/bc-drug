7c79197f2926c4fb99323f17a0a8e790c3f53dc8
Me <getdaniel>
2023-05-22 11:21:31 +0300

    colored the pIC50 value of bar

--- source/model.py
+++ source/model.py
@@ -4,10 +4,16 @@ import streamlit as st
 
 from source.file_download import filedownload
 
+# Define color codes
+green_color = '#00FF00'
+yellow_color = '#FFFF00'
+red_color = '#FF0000'
+
 # Model building
 def build_model(load_data, input_data):
     if input_data.shape[0] == 0:
-        st.info("Input data has zero samples/ No descriptor Value. Please provide valid input data.")
+        st.info(
+            "Input data has zero samples/ No descriptor Value. Please provide valid input data.")
     else:
         # Reads in saved regression model
         load_model = pickle.load(open('ML/aromatase.pkl', 'rb'))
@@ -23,7 +29,13 @@ def build_model(load_data, input_data):
         # Draw a bar chart with molecule_name as X-axis and prediction_output as Y-axis
         st.header('**Graphical Prediction Output**')
         chart_data = df.set_index('molecule_name')
-        st.bar_chart(chart_data)
+
+        # Create a new column for bar color based on pIC50 values
+        chart_data['color'] = chart_data['pIC50'].apply(
+            lambda x: green_color if x >= 6.5 else (yellow_color if 4.5 <= x < 6.5 else red_color))
+
+        # Plot the bar chart with color-coded bars
+        st.bar_chart(chart_data['pIC50'], color=chart_data['color'])
 
         # download the predicted csv file
         st.markdown(filedownload(df), unsafe_allow_html=True)
