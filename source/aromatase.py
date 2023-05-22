7c4dba8cf3dad1047934805ee9fb39c00dacada6
Me <getdaniel>
2023-05-22 21:41:43 +0300

    try animated from rcsb

--- source/aromatase.py
+++ source/aromatase.py
@@ -1,8 +1,17 @@
 import streamlit as st
 from rdkit import Chem
 from rdkit.Chem import Draw
+import streamlit.components.v1 as components
+
 
 def aromatase_structure():
+    # Define the URL of the animated 3D view
+    view_url = 'https://www.rcsb.org/3d-view/3EQM'
+
+    # Display the animated 3D view using Streamlit components
+    st.header('Aromatase Enzyme 3D Structure')
+    components.iframe(view_url, height=800)
+    
     # Define the SMILES notation for aromatase
     smiles = "CC1=CC(=CC=C1CCC(=O)NC2=CC=C(C=C2)Cl)O"
 
