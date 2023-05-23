a382ea1691eb5e80a49bbf7619221f390c41b3e1
Me <getdaniel>
2023-05-24 01:14:02 +0300

    confirm to warning

--- source/logout.py
+++ source/logout.py
@@ -4,4 +4,4 @@ import streamlit as st
 def handle_logout(modal):
     if modal.is_open():
         with modal.container():
-            st.confirm("Are you sure you want to log out?")
+            st.warning("Are you sure you want to log out?")
