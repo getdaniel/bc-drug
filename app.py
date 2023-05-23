e23b72954ff0b3cddb37829e8035bec4a365b2eb
Me <getdaniel>
2023-05-23 23:13:36 +0300

    update the func

--- app.py
+++ app.py
@@ -38,6 +38,9 @@ uploaded_file = None
 # New Web button callback
 def on_new_web_button_click():
     global uploaded_file
+    if uploaded_file is not None:
+        # Remove the uploaded file from the system
+        os.remove(uploaded_file.name)
     uploaded_file = None
 
 # Add buttons to sidebar
