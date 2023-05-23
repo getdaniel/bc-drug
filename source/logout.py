0f9ee46913ae5f7c01bc0cd21ee75b53063d1af2
Me <getdaniel>
2023-05-24 01:08:52 +0300

    st.confirm

--- source/logout.py
+++ source/logout.py
@@ -4,4 +4,4 @@ import streamlit as st
 def handle_logout(modal):
     if modal.is_open():
         with modal.container():
-            st.info("Logged out")
+            st.confirm("Are you sure you want to log out?")
