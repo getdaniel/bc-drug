b6dd6c17e211946d4041ab43c35366a4b21c1f43
Me <getdaniel>
2023-05-26 00:45:25 +0300

    try to see the spectrum

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ id = st.text_input("Enter your protein ID", value="3EQM", help="Example: 3EQM")
 components.html("""
                 <script src="https://3Dmol.org/build/3Dmol-min.js"></script>     
                 <script src="https://3Dmol.org/build/3Dmol.ui-min.js"></script>  
-                <div style="height: 400px; width: 600px; position: relative;" class='viewer_3Dmoljs' data-pdb={} data-backgroundcolor='0xffffff' data-style='stick' data-ui='true' data-style1='cartoon:color=spectrum' data-spin='axis:y;speed:1'></div>
+                <div style="height: 400px; width: 600px; position: relative;" class='viewer_3Dmoljs' data-pdb={} data-backgroundcolor='0xffffff' data-style='stick' data-style1='cartoon:color=spectrum' data-spin='axis:y;speed:1' data-ui='true'></div>
                 """.format(id), width=500, height=500
             )
 
