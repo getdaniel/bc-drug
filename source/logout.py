86a844cd8f47999372ad0f0398fe2d503344400e
Me <getdaniel>
2023-05-25 11:56:28 +0300

    use flex style

--- source/logout.py
+++ source/logout.py
@@ -4,9 +4,10 @@ def handle_logout(modal):
     if modal.is_open():
         with modal.container():
             st.warning("Are you sure you want to log out?")
-            button_container = st.container()
-            
-            with button_container:
-                st.write('<style>div.stButton > button:first-child { margin-right: 10px; }</style>', unsafe_allow_html=True)
-                st.button("Cancel")
-                st.button("OK")
+            st.write(
+                '<div style="display: flex; justify-content: space-between;">'
+                '<button style="margin-right: 10px;">Cancel</button>'
+                '<button>OK</button>'
+                '</div>',
+                unsafe_allow_html=True
+            )
