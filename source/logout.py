cd2f290d9b430df424f61d40baee951726b2d2a2
Me <getdaniel>
2023-05-25 21:31:09 +0300

    update to get func

--- source/logout.py
+++ source/logout.py
@@ -4,22 +4,27 @@ def handle_logout(modal):
     if modal.is_open():
         with modal.container():
             st.warning("Are you sure you want to log out?")
+            
             st.write(
-                '''
-                <div style="display: flex; justify-content: space-between;">
-                    <button style="margin-right: 25px;" onclick="closeModal()">Cancel</button>
-                    <button onclick="redirectToLink()">OK</button>
-                </div>
-                <script>
-                    function closeModal() {
-                        // Close the modal
-                        Streamlit.setComponentValue(false);
-                    }
-                    function redirectToLink() {
-                        // Redirect to external link without opening in a new tab
-                        window.location.href = "https://getdaniel.github.io/bc-drug/";
-                    }
-                </script>
-                ''',
+                '<style>.streamlit-modal-content { max-width: 600px !important; width: 80% !important; }</style>',
                 unsafe_allow_html=True
             )
+            
+            button_container = st.beta_container()
+            
+            with button_container:
+                st.write(
+                    '<style>div.stButton > button:first-child { margin-right: 10px; }</style>',
+                    unsafe_allow_html=True
+                )
+                
+                cancel_clicked = st.button("Cancel")
+                ok_clicked = st.button("OK")
+
+                if cancel_clicked:
+                    # Handle cancel logic here
+                    modal.close()
+
+                if ok_clicked:
+                    # Handle OK logic here
+                    modal.close()
