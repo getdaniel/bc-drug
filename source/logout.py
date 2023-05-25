1328707d212df60afe48efc92c8d59e610d6b7b9
Me <getdaniel>
2023-05-25 11:53:55 +0300

    use container

--- source/logout.py
+++ source/logout.py
@@ -4,10 +4,9 @@ def handle_logout(modal):
     if modal.is_open():
         with modal.container():
             st.warning("Are you sure you want to log out?")
-            button_col1, button_col2, button_col3 = st.columns([1, None, 1])
-            if button_col1.button("Cancel"):
-                # Handle cancel logic here
-                pass
-            if button_col2.button("OK"):
-                # Handle OK logic here
-                pass
+            button_container = st.container()
+            
+            with button_container:
+                st.write('<style>div.stButton > button:first-child { margin-right: 10px; }</style>', unsafe_allow_html=True)
+                st.button("Cancel")
+                st.button("OK")
