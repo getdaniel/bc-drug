c542fad221097d9e52bfdce81e5b6869b8a1f22c
Me <getdaniel>
2023-05-25 11:46:22 +0300

    put in column

--- source/logout.py
+++ source/logout.py
@@ -1,9 +1,13 @@
 import streamlit as st
 
-
 def handle_logout(modal):
     if modal.is_open():
         with modal.container():
             st.warning("Are you sure you want to log out?")
-            st.button("Cancel")
-            st.button("OK")
+            button_col1, button_col2 = st.columns(2)
+            if button_col1.button("Cancel"):
+                # Handle cancel logic here
+                pass
+            if button_col2.button("OK"):
+                # Handle OK logic here
+                pass
