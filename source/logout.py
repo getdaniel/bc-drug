a8f03e3fb8cc1ac444d93c13413a67ff3ad1a985
Me <getdaniel>
2023-05-25 12:02:10 +0300

    add functionality for buttons

--- source/logout.py
+++ source/logout.py
@@ -5,9 +5,21 @@ def handle_logout(modal):
         with modal.container():
             st.warning("Are you sure you want to log out?")
             st.write(
-                '<div style="display: flex; justify-content: space-between;">'
-                '<button style="margin-right: 10px;">Cancel</button>'
-                '<button>OK</button>'
-                '</div>',
+                '''
+                <div style="display: flex; justify-content: space-between;">
+                    <button style="margin-right: 25px;" onclick="closeModal()">Cancel</button>
+                    <button onclick="redirectToLink()">OK</button>
+                </div>
+                <script>
+                    function closeModal() {
+                        // Close the modal
+                        Streamlit.setComponentValue(false);
+                    }
+                    function redirectToLink() {
+                        // Redirect to external link without opening in a new tab
+                        window.location.href = "https://getdaniel.github.io/bc-drug/";
+                    }
+                </script>
+                ''',
                 unsafe_allow_html=True
             )
