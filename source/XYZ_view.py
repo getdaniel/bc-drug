4eacaa6d1f7cad623406c1ff8d1968d98f62aa2d
Me <getdaniel>
2023-05-27 00:06:15 +0300

    update and enlarge

--- source/XYZ_view.py
+++ source/XYZ_view.py
@@ -17,5 +17,5 @@ def show_3d_visualization():
     components.html("""
         <script src="https://3Dmol.org/build/3Dmol-min.js"></script>     
         <script src="https://3Dmol.org/build/3Dmol.ui-min.js"></script>  
-        <div style="height: 400px; width: 600px; position: relative;" class='viewer_3Dmoljs' data-pdb={} data-backgroundcolor='0xffffff' data-style='stick' data-style1='cartoon:color=spectrum' data-spin='axis:y;speed:1' data-ui='true'></div>
-    """.format(id), width=500, height=1000)
+        <div style="height: 400px; width: 1000px; position: relative;" class='viewer_3Dmoljs' data-pdb={} data-backgroundcolor='0xffffff' data-style='stick' data-style1='cartoon:color=spectrum' data-spin='axis:y;speed:1' data-ui='true'></div>
+    """.format(id), width=500, height=500)
