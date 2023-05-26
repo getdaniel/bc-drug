b9942d0d4e47a0f8666760fc5c0355f419484fa9
Me <getdaniel>
2023-05-26 23:57:40 +0300

    return to the original

--- source/XYZ_view.py
+++ source/XYZ_view.py
@@ -14,35 +14,8 @@ def show_3d_visualization():
                     help="Example: 3EQM or Vist [RSCB Website](https://www.rcsb.org) for more"
                 )
     
-    # Define the CSS styles for the viewer container
-    viewer_styles = """
-    .viewer-container {
-        width: 100%;
-        height: 0;
-        padding-bottom: 75%; /* Adjust the aspect ratio based on your needs */
-        position: relative;
-    }
-
-    .viewer_3Dmoljs {
-        width: 100%;
-        height: 100%;
-        position: absolute;
-        top: 0;
-        left: 0;
-    }
-    """
-
-    # Add the CSS styles to the page
-    st.markdown(f"<style>{viewer_styles}</style>", unsafe_allow_html=True)
-
-    # Generate the HTML code for the viewer
-    html_code = """
-    <div class='viewer-container'>
-        <script src="https://3Dmol.org/build/3Dmol-min.js"></script>
-        <script src="https://3Dmol.org/build/3Dmol.ui-min.js"></script>
-        <div class='viewer_3Dmoljs' data-pdb='{}' data-backgroundcolor='0xffffff' data-style='stick' data-style1='cartoon:color=spectrum' data-spin='axis:y;speed:1' data-ui='true'></div>
-    </div>
-    """.format(id)
-
-    # Render the HTML code using components.html
-    components.html(html_code, width=500, height=500)
+    components.html("""
+        <script src="https://3Dmol.org/build/3Dmol-min.js"></script>     
+        <script src="https://3Dmol.org/build/3Dmol.ui-min.js"></script>  
+        <div style="height: 400px; width: 600px; position: relative;" class='viewer_3Dmoljs' data-pdb={} data-backgroundcolor='0xffffff' data-style='stick' data-style1='cartoon:color=spectrum' data-spin='axis:y;speed:1' data-ui='true'></div>
+    """.format(id), width=500, height=500)
