ab92d484a612d6faa4d77d4b0a314b90b4eaad32
Me <getdaniel>
2023-05-27 00:18:07 +0300

    update with new size

--- source/XYZ_view.py
+++ source/XYZ_view.py
@@ -17,5 +17,5 @@ def show_3d_visualization():
     components.html("""
         <script src="https://3Dmol.org/build/3Dmol-min.js"></script>     
         <script src="https://3Dmol.org/build/3Dmol.ui-min.js"></script>  
-        <div style="height: 400px; width: 400px; position: relative;" class='viewer_3Dmoljs' data-pdb={} data-backgroundcolor='0xffffff' data-style='stick' data-style1='cartoon:color=spectrum' data-spin='axis:y;speed:1' data-ui='true'></div>
+        <div style="height: 400px; width: 800px; position: relative;" class='viewer_3Dmoljs' data-pdb={} data-backgroundcolor='0xffffff' data-style='stick' data-style1='cartoon:color=spectrum' data-spin='axis:y;speed:1' data-ui='true'></div>
     """.format(id), width=800, height=600)
