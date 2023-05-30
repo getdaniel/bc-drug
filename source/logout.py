1cad4bf14ed0171cab5a91bfe878889dcb59f7f1
Me <getdaniel>
2023-05-30 16:29:08 +0300

    use css style

--- source/logout.py
+++ source/logout.py
@@ -5,15 +5,25 @@ def handle_logout(modal):
         with modal.container():
             st.warning("Are you sure you want to log out?")
             
-            # Add buttons in one row
+            # Add buttons in one row with CSS styling
+            st.markdown("""
+            <style>
+            .button-row {
+                display: flex;
+                justify-content: space-between;
+            }
+            </style>
+            """, unsafe_allow_html=True)
+            
+            # Button row
             col1, col2 = st.columns(2)
-            
-            # Cancel button in left corner
-            if col1.button("No", type="secondary", use_column_width=True):
-                modal.close()
-            
-            if col2.button("OK", type="primary", use_column_width=True):
-                # Redirect to another link
-                logout_url = "https://getdaniel.github.io/bc-drug/"  # Replace with the desired link
-                redirect_js = f"window.location.href = '{logout_url}';"
-                st.write(f'<script>{redirect_js}</script>', unsafe_allow_html=True)
+            with st.markdown('<div class="button-row">', unsafe_allow_html=True):
+                if col1.button("No", type="secondary"):
+                    modal.close()
+                
+                if col2.button("OK", type="primary"):
+                    # Redirect to another link
+                    logout_url = "https://getdaniel.github.io/bc-drug/"  # Replace with the desired link
+                    redirect_js = f"window.location.href = '{logout_url}';"
+                    st.write(f'<script>{redirect_js}</script>', unsafe_allow_html=True)
+            st.markdown('</div>', unsafe_allow_html=True)
