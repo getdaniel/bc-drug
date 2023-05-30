a49fe9cf82154c40648fbb83c0c778a37647d892
Me <getdaniel>
2023-05-30 15:40:42 +0300

    decrease one space

--- source/logout.py
+++ source/logout.py
@@ -5,7 +5,7 @@ def handle_logout(modal):
         with modal.container():
             st.warning("Are you sure you want to log out?")
             # Add buttons in one row
-            col1, col20, col21, col22, col23, col24, col25, col26, col27, col28, col3 = st.columns(11)
+            col1, col20, col21, col22, col23, col24, col25, col26, col27, col3 = st.columns(10)
             
             # Cancel button in left corner
             if col1.button("Cancel"):
