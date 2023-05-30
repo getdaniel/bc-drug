be1c59fcf319ea434b06de5fa65651801219e3c5
Me <getdaniel>
2023-05-30 16:25:52 +0300

    use column width

--- source/logout.py
+++ source/logout.py
@@ -4,14 +4,15 @@ def handle_logout(modal):
     if modal.is_open():
         with modal.container():
             st.warning("Are you sure you want to log out?")
+            
             # Add buttons in one row
             col1, col2 = st.columns(2)
             
             # Cancel button in left corner
-            if col1.button("No", type="secondary"):
+            if col1.button("No", type="secondary", use_column_width=True):
                 modal.close()
             
-            if col2.button("OK", type="primary"):
+            if col2.button("OK", type="primary", use_column_width=True):
                 # Redirect to another link
                 logout_url = "https://getdaniel.github.io/bc-drug/"  # Replace with the desired link
                 redirect_js = f"window.location.href = '{logout_url}';"
