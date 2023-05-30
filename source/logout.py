00d92536c9d91b809e2016adaa628522bc8a9a83
Me <getdaniel>
2023-05-30 20:06:58 +0300

    sole button

--- source/logout.py
+++ source/logout.py
@@ -6,19 +6,15 @@ def handle_logout(modal):
             st.warning("Are you sure you want to log out?")
             
             # Add buttons in one row
-            col1, col10, col11, col12, col2 = st.columns(5)
-            
-            # Cancel button in left corner
-            if col1.button("No"):
-                modal.close()
+            col1 = st.columns(1)
             
             # OK button in right corner
-            if col2:
+            if col1:
                 # Redirect to another link
                 logout_url = "https://getdaniel.github.io/bc-drug/"  # Replace with the desired link
                 
                 # Generate HTML for opening link in new tab
-                new_tab_html = f'<a href="{logout_url}" target="_self" rel="noopener noreferrer">OK</a>'
+                new_tab_html = f'<a href="{logout_url}" target="_self" rel="noopener noreferrer"><button>OK</button></a>'
                 
                 # Display the HTML
                 st.write(new_tab_html, unsafe_allow_html=True)
