977b8803779570435ede74f50a4368ad96e121dc
Me <getdaniel>
2023-05-30 16:34:37 +0300

    return to the original

--- source/logout.py
+++ source/logout.py
@@ -5,29 +5,15 @@ def handle_logout(modal):
         with modal.container():
             st.warning("Are you sure you want to log out?")
             
-            # Add buttons in one row with HTML and CSS
-            st.markdown("""
-            <style>
-            .button-container {
-                display: flex;
-                justify-content: space-between;
-            }
-            </style>
-            """, unsafe_allow_html=True)
+            # Add buttons in one row
+            col1, col2 = st.columns(2)
             
-            # Button container
-            st.markdown('<div class="button-container">', unsafe_allow_html=True)
-            
-            # Cancel button
-            if st.button("No", key="cancel_button"):
+            # Cancel button in left corner
+            if col1.button("No", type="secondary"):
                 modal.close()
             
-            # OK button
-            if st.button("OK", key="ok_button"):
+            if col2.button("OK", type="primary"):
                 # Redirect to another link
                 logout_url = "https://getdaniel.github.io/bc-drug/"  # Replace with the desired link
                 redirect_js = f"window.location.href = '{logout_url}';"
                 st.write(f'<script>{redirect_js}</script>', unsafe_allow_html=True)
-            
-            # Close button container
-            st.markdown('</div>', unsafe_allow_html=True)
