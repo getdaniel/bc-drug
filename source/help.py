9cac88f9d4d59c24cc0c44153c47e19cb5816cbc
Me <getdaniel>
2023-05-30 12:25:56 +0300

    update with component

--- source/help.py
+++ source/help.py
@@ -3,36 +3,39 @@ import streamlit.components.v1 as components
 
 
 def help_page():
-    components.html(
-        """
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    # Importing the necessary CDNs
+    st.markdown("""
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    """, unsafe_allow_html=True)
 
-            <div class="modal" tabindex="-1" role="dialog">
-                <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title">Logout Confirmation</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <div class="modal-body">
-                            <p>Are you sure you want to logout?</p>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-                            <button id="logout-btn" type="button" class="btn btn-primary">Logout</button>
+    # Show the modal when the button is clicked
+    components.html("""
+                    <div class="modal" tabindex="-1" role="dialog">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title">Logout Confirmation</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+                                    <p>Are you sure you want to logout?</p>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                                    <button id="logout-btn" type="button" class="btn btn-primary">Logout</button>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </div>
-            <script>
-                // Add event listener to the Logout button
-                document.getElementById('logout-btn').addEventListener('click', function() {
-                    // Redirect to the new link
-                    window.location.href = 'https://example.com'; // Replace with your desired link
-                });
-            </script>
-        """)
+                    <script>
+                        // Add event listener to the Logout button
+                        document.getElementById('logout-btn').addEventListener('click', function() {
+                            // Redirect to the new link
+                            window.location.href = 'https://example.com'; // Replace with your desired link
+                        });
+                    </script>
+                    """, height=0)
