a58f0a35b78540643d6ba021994fe705a1b07863
Me <getdaniel>
2023-05-30 16:31:51 +0300

    try another wy

--- source/logout.py
+++ source/logout.py
@@ -5,25 +5,29 @@ def handle_logout(modal):
         with modal.container():
             st.warning("Are you sure you want to log out?")
             
-            # Add buttons in one row with CSS styling
+            # Add buttons in one row with HTML and CSS
             st.markdown("""
             <style>
-            .button-row {
+            .button-container {
                 display: flex;
                 justify-content: space-between;
             }
             </style>
             """, unsafe_allow_html=True)
             
-            # Button row
-            col1, col2 = st.columns(2)
-            with st.markdown('<div class="button-row">', unsafe_allow_html=True):
-                if col1.button("No", type="secondary"):
-                    modal.close()
-                
-                if col2.button("OK", type="primary"):
-                    # Redirect to another link
-                    logout_url = "https://getdaniel.github.io/bc-drug/"  # Replace with the desired link
-                    redirect_js = f"window.location.href = '{logout_url}';"
-                    st.write(f'<script>{redirect_js}</script>', unsafe_allow_html=True)
+            # Button container
+            st.markdown('<div class="button-container">', unsafe_allow_html=True)
+            
+            # Cancel button
+            if st.button("No", key="cancel_button"):
+                modal.close()
+            
+            # OK button
+            if st.button("OK", key="ok_button"):
+                # Redirect to another link
+                logout_url = "https://getdaniel.github.io/bc-drug/"  # Replace with the desired link
+                redirect_js = f"window.location.href = '{logout_url}';"
+                st.write(f'<script>{redirect_js}</script>', unsafe_allow_html=True)
+            
+            # Close button container
             st.markdown('</div>', unsafe_allow_html=True)
