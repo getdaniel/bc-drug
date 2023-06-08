b252516dacc83f6afebc12414f453ae9bd482dfa
Me <getdaniel>
2023-06-09 00:10:01 +0300

    eye for register page pwd

--- js/eye.js
+++ js/eye.js
@@ -1,12 +1,12 @@
-function togglePasswordVisibility() {
-    const passwordInput = document.getElementById('password');
-    const passwordToggle = document.querySelector('.password-toggle');
-  
-    if (passwordInput.type === 'password') {
-      passwordInput.type = 'text';
-      passwordToggle.innerHTML = '<i class="fa fa-eye-slash"></i>';
-    } else {
-      passwordInput.type = 'password';
-      passwordToggle.innerHTML = '<i class="fa fa-eye"></i>';
-    }
-  }  
+function togglePasswordVisibility(inputId) {
+  const passwordInput = document.getElementById(inputId);
+  const passwordToggle = passwordInput.nextElementSibling;
+
+  if (passwordInput.type === 'password') {
+    passwordInput.type = 'text';
+    passwordToggle.innerHTML = '<i class="fa fa-eye-slash"></i>';
+  } else {
+    passwordInput.type = 'password';
+    passwordToggle.innerHTML = '<i class="fa fa-eye"></i>';
+  }
+}
