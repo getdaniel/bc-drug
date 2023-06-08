ef6fb108be9e41ff6a5f9f9e22997a84f92df40b
Me <getdaniel>
2023-06-08 21:59:07 +0300

    add spinner

--- js/login.js
+++ js/login.js
@@ -4,6 +4,18 @@ const passwordInput = document.getElementById('password');
 const loginForm = document.querySelector('.login-form');
 const messageDiv = document.getElementById('message');
 
+// Function to show the spinning/loading indicator
+function showSpinner() {
+  // Add a CSS class to display the spinning/loading effect
+  messageDiv.classList.add('spinner');
+}
+
+// Function to hide the spinning/loading indicator
+function hideSpinner() {
+  // Remove the CSS class to hide the spinning/loading effect
+  messageDiv.classList.remove('spinner');
+}
+
 // Add an event listener to the login form submit button
 loginForm.addEventListener('submit', (event) => {
   event.preventDefault(); // Prevent the form from submitting
@@ -11,14 +23,22 @@ loginForm.addEventListener('submit', (event) => {
   const email = emailInput.value;
   const password = passwordInput.value;
 
+  // Show the spinning/loading indicator
+  showSpinner();
+
   // Sign in the user with email and password
   firebase.auth().signInWithEmailAndPassword(email, password)
     .then(() => {
-      messageDiv.textContent = 'Login successful!';
+      // Hide the spinning/loading indicator
+      hideSpinner();
+
       loginForm.reset(); // Clear the form
       window.location.href = 'https://getdaniel-bc-drug.streamlit.app/'; // Redirect to Streamlit app
     })
     .catch((error) => {
+      // Hide the spinning/loading indicator
+      hideSpinner();
+
       if (error.code === 'auth/wrong-password') {
         messageDiv.textContent = 'Wrong password';
       } else if (error.code === 'auth/user-not-found') {
