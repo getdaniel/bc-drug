38c53118c0b02f90398f1da19575a0dac976dea6
Me <getdaniel>
2023-06-10 21:35:34 +0300

    change to markdown instead

--- source/feedback.py
+++ source/feedback.py
@@ -4,8 +4,16 @@ import streamlit as st
 def handle_user_feedback(modal):
     if modal.is_open():
         with modal.container():
-            email = st.text_input("Email")
-            message = st.text_area("Message")
-            if st.button("Submit", use_container_width=True):
-                # Handle the feedback submission
-                st.success("Feedback submitted successfully.")
+            # email = st.text_input("Email")
+            # message = st.text_area("Message")
+            # if st.button("Submit", use_container_width=True):
+            #     # Handle the feedback submission
+            #     st.success("Feedback submitted successfully.")
+            st.markdown(
+                """
+                    <h1>Feedback Form</h1>
+                    <input type="text" id="email" placeholder="Email">
+                    <textarea id="message" placeholder="Message"></textarea>
+                    <button onclick="saveFeedback()">Submit</button>
+
+                """)
