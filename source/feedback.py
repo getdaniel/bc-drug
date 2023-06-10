73336928a07046eb4a901263e9f0b4d6b45051d2
Me <getdaniel>
2023-06-10 21:49:10 +0300

    feedback with html file

--- source/feedback.py
+++ source/feedback.py
@@ -9,11 +9,25 @@ def handle_user_feedback(modal):
             # if st.button("Submit", use_container_width=True):
             #     # Handle the feedback submission
             #     st.success("Feedback submitted successfully.")
-            st.markdown(
-                """
-                    <h1>Feedback Form</h1>
-                    <input type="text" id="email" placeholder="Email">
-                    <textarea id="message" placeholder="Message"></textarea>
-                    <button onclick="saveFeedback()">Submit</button>
+            # HTML code for the form
+            html_form = """
+            <div class="container">
+            <h1>Email Form</h1>
+            <form>
+                <div class="form-group">
+                <label for="email">Email:</label>
+                <input type="email" id="email" placeholder="Enter your email" required>
+                </div>
+                <div class="form-group">
+                <label for="message">Message:</label>
+                <textarea id="message" placeholder="Enter your message" rows="4" required></textarea>
+                </div>
+                <div class="form-group">
+                <button type="submit">Submit</button>
+                </div>
+            </form>
+            </div>
+            """
 
-                """)
+            # Render the HTML form
+            st.components.v1.html(html_form, width=500, height=300)
