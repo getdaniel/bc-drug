321fb8da46b66f6595c47d4d435e196b5cb6ebfb
Me <getdaniel>
2023-06-11 12:36:16 +0300

    update with fine button

--- source/feedback.py
+++ source/feedback.py
@@ -1,72 +1,80 @@
 import streamlit as st
 
-
 def handle_user_feedback(modal):
     if modal.is_open():
         with modal.container():
-            # email = st.text_input("Email")
-            # message = st.text_area("Message")
-            # if st.button("Submit", use_container_width=True):
-            #     # Handle the feedback submission
-            #     st.success("Feedback submitted successfully.")
-            # HTML code for the form
             html_form = """
-                <style>
-                    .login-form {
-                        max-width: 350px;
-                        margin: 0 auto;
-                        width: 100%;
-                        padding: 5px;
-                        display: flex;
-                        flex-direction: column;
-                    }
+                <!-- HTML code for the form -->
+                <div id="feedback-container">
+                    <style>
+                        .login-form {
+                            max-width: 350px;
+                            margin: 0 auto;
+                            width: 100%;
+                            padding: 5px;
+                            display: flex;
+                            flex-direction: column;
+                        }
 
-                    .login-form label {
-                        display: block;
-                        margin-bottom: 5px;
-                        font-weight: bold;
-                    }
+                        .login-form label {
+                            display: block;
+                            margin-bottom: 5px;
+                            font-weight: bold;
+                        }
 
-                    .login-form input[type="email"],
-                    .login-form textarea {
-                        width: 100%;
-                        padding: 10px;
-                        border: 1px solid #ccc;
-                        border-radius: 5px;
-                        margin-bottom: 5px;
-                    }
+                        .login-form input[type="email"],
+                        .login-form textarea {
+                            width: 100%;
+                            padding: 10px;
+                            border: 1px solid #ccc;
+                            border-radius: 5px;
+                            margin-bottom: 5px;
+                        }
 
-                    .login-form input[type="submit"] {
-                        background-color: #4CAF50;
-                        color: white;
-                        border: none;
-                        padding: 10px 20px;
-                        border-radius: 5px;
-                        cursor: pointer;
-                        width: 100%;
-                    }
+                        .login-form input[type="submit"] {
+                            background-color: #4CAF50;
+                            color: white;
+                            border: none;
+                            padding: 10px 20px;
+                            border-radius: 5px;
+                            cursor: pointer;
+                            width: 100%;
+                        }
 
-                    .login-form input[type="submit"]:hover {
-                        background-color: #45a049;
-                    }
-                </style>
+                        .login-form input[type="submit"]:hover {
+                            background-color: #45a049;
+                        }
 
-                <div style="display: flex; justify-content: center; align-items: center; height: 100vh;">
-                    <form class="login-form" id="feedback-form">
-                        <h3 style="text-align: center;">Send Us Feedback</h3>
-                        <label for="email">
-                            Email:
-                            <input type="email" id="email" name="email" placeholder="Enter your email" required>
-                        </label>
-                        <label for="message">
-                            Message:
-                            <textarea id="message" placeholder="Enter your message" rows="4" required></textarea>
-                        </label>
-                        <input type="submit" value="Submit">
-                    </form>
+                        .success-message {
+                            color: green;
+                            margin-top: 10px;
+                        }
+                    </style>
+
+                    <div style="display: flex; justify-content: center; align-items: center; height: 100vh;">
+                        <form class="login-form" id="feedback-form">
+                            <h3 style="text-align: center;">Send Us Feedback</h3>
+                            <label for="email">
+                                Email:
+                                <input type="email" id="email" name="email" placeholder="Enter your email" required>
+                            </label>
+                            <label for="message">
+                                Message:
+                                <textarea id="message" placeholder="Enter your message" rows="4" required></textarea>
+                            </label>
+                            <input type="submit" value="Submit">
+
+                            <!-- Success message container -->
+                            <div id="message-container"></div>
+                        </form>
+                    </div>
+
+                    
                 </div>
 
-                <script src="https://cdnjs.cloudflare.com/ajax/libs/firebase/7.14.1-0/firebase.js"></script>
+                <!-- Firebase SDK and JavaScript code -->
+                <script src="https://www.gstatic.com/firebasejs/9.1.3/firebase-app.js"></script>
+                <script src="https://www.gstatic.com/firebasejs/9.1.3/firebase-database.js"></script>
                 <script>
                     const firebaseConfig = {
                         apiKey: "AIzaSyDWlsnIKRS93tzZkb58NjvW0c7BIkUMwvA",
@@ -102,7 +110,13 @@ def handle_user_feedback(modal):
 
                             // Reset form fields
                             form.reset();
-                            alert("Feedback submitted successfully.");
+                            
+                            // Display success message
+                            var messageContainer = document.getElementById("message-container");
+                            var successMessage = document.createElement("div");
+                            successMessage.className = "success-message";
+                            successMessage.textContent = "Feedback submitted successfully.";
+                            messageContainer.appendChild(successMessage);
                         });
                     });
                 </script>
